f83ac17149cd8dd8798e169beafe98256f689c19
unknown <airmp13@yahoo.com>
2023-08-05 17:30:10 +0330

    Unique indexes

---
+++ pr020507-2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace pr020507_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 0,0, 1, 1, 1, 2, 2, 3, 3, 4,5,5,5,6 };
+
+            Console.WriteLine("Number of Uniqe Items is: "+ CountOfUniqueItems(nums));
+        }
+    
+        public static int CountOfUniqueItems(int[] nums)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(nums[0]);
+            int buf= 0;
+            for (int i = 1;i<nums.Length;i++)
+            {
+                if (stack.Peek() != nums[i]) stack.Push(nums[i]);
+
+                
+
+            }
+
+            return stack.Count();
+        }
+    
+    }
+}
